50519714162377de0464f2a8a9cf5e3303f3c865
Harsh-7705 <harshaiginno@gmail.com>
2025-07-18 20:44:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // ✅ Required for `next start` on Render
 };
 
 export default nextConfig;
